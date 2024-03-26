c9989bece2f072fdf481180190d2993a48ca3e3c
Jonas Dias <zdias1413@gmail.com>
2024-03-26 12:00:45 -0300

    feat: add services

---
+++ unit-tests/src/main/java/com/unittests/services/UserService.java
@@ -0,0 +1,36 @@
+package com.unittests.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.unittests.exceptions.UserException;
+import com.unittests.models.User;
+import com.unittests.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+      @Autowired
+      private UserRepository userRepository;
+
+      public User save(User user) {
+            return userRepository.save(user);
+      }
+
+      public User update(User user) {
+            return userRepository.save(user);
+      }
+
+      public void delete(Long id) {
+            userRepository.deleteById(id);
+      }
+
+      public List<User> findAll() {
+            return userRepository.findAll();
+      }
+
+      public User findByUsername(String username) {
+            return userRepository.findByUsername(username)
+                        .orElseThrow(() -> new UserException("User not found"));
+      }
+}

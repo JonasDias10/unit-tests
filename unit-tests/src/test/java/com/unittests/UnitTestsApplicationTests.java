f50e5e3b1a8f29efaa5066e2ed55493f570348f8
Jonas Dias <zdias1413@gmail.com>
2024-03-27 00:15:27 -0300

    test: add tests to save and update user

--- unit-tests/src/test/java/com/unittests/UnitTestsApplicationTests.java
+++ unit-tests/src/test/java/com/unittests/UnitTestsApplicationTests.java
@@ -2,6 +2,9 @@ package com.unittests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
@@ -24,13 +27,53 @@ class UnitTestsApplicationTests {
 	@Test
 	void testFindByUserName() {
 		String username = "JonasDias";
+		String password = "12345678";
 
-		when(userRepositoryMock.findByUsername(username)).thenReturn(Optional.of(new User(username)));
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
 
-		User finderUser = userServiceMock.findByUsername("JonasDias");
+		when(userRepositoryMock.findByUsername(username)).thenReturn(Optional.of(user));
+		User finderUser = userServiceMock.findByUsername(username);
 
 		assertNotNull(finderUser);
 		assertEquals(username, finderUser.getUsername());
+	}
+
+	@Test
+	void testSaveUser() {
+		User user = new User();
+		user.setUsername("JonasDias");
+		user.setPassword("12345678");
+
+		when(userRepositoryMock.save(user)).thenReturn(user);
+		User savedUser = userServiceMock.save(user);
+
+		verify(userRepositoryMock, times(1)).save(user);
+		assertEquals("JonasDias", savedUser.getUsername());
+		assertEquals("12345678", savedUser.getPassword());
+	}
+
+	@Test
+	void testUpdateUserWhenUserAlreadyExists() {
+		User savedUser = new User();
+		savedUser.setId(1L);
+		savedUser.setUsername("Jonas");
+		savedUser.setPassword("1234");
+
+		User updatedUser = new User();
+		updatedUser.setId(1L);
+		updatedUser.setUsername("Jonas Dias");
+		updatedUser.setPassword("12345678");
+
+		when(userRepositoryMock.save(any(User.class))).thenReturn(updatedUser);
+
+		User result = userServiceMock.update(updatedUser);
+
+		verify(userRepositoryMock, times(1)).save(updatedUser);
+
+		assertEquals("Jonas Dias", result.getUsername());
+		assertEquals("12345678", result.getPassword());
 	}
 
 }
